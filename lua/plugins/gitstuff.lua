53ba4887392737c03e5e96b88de604b080709448
wiha97 <wiha97@duck.com>
2025-02-06 13:03:53 +0100

    Better git blame

--- lua/plugins/gitstuff.lua
+++ lua/plugins/gitstuff.lua
@@ -5,7 +5,9 @@ return {
   {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+        current_line_blame_formatter = '<!> <author_time:%R> - "<summary>" (<author>)'
+      })
   end,
   }
 }
