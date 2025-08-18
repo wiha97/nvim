1973dcdc69b8d1cd4daf5b3419ad3298d43a8639
wiha97 <wiha97@duck.com>
2025-08-18 22:35:24 +0200

    Changed icon in gitstuff

--- lua/plugins/gitstuff.lua
+++ lua/plugins/gitstuff.lua
@@ -6,7 +6,7 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup({
-        current_line_blame_formatter = '<!> <author_time:%R> - "<summary>" (<author>)'
+        current_line_blame_formatter = ' <author_time:%R> - "<summary>" (<author>)'
       })
   end,
   },
