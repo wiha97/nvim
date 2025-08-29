198c99cd75570f704984e0b352bb4bb133bedcbf
wiha97 <wiha97@duck.com>
2025-08-29 14:14:08 +0200

    Improved image for markdown

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -3,6 +3,11 @@ return {
     "3rd/image.nvim",
     build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
     opts = {
+      integrations = {
+        markdown = {
+          only_render_image_at_cursor = true,
+        },
+      },
       processor = "magick_cli",
     }
   }
